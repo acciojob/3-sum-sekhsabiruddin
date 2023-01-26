da9b987d36ade2483f40858d77caff402ee6d48d
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-26 22:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function threeSum(arr, target) {
+
+function threeSum(array, target) {
   //your code here
+	 let closestSum = 0;
+    let minDiff = Infinity;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            for (let k = j + 1; k < array.length; k++) {
+                let sum = array[i] + array[j] + array[k];
+                let diff = Math.abs(target - sum);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    closestSum = sum;
+                }
+            }
+        }
+    }
+
+    return closestSum;
 }
+
+
 
 module.exports = threeSum;
